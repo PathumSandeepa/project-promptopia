060b5047c851818baf7144489352493896fdbc25
Pathum Sandeepa <pathumsandeepa13@gmail.com>
2024-08-30 22:23:44 +0530

    navbar and google api setup

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,26 @@
 import "@styles/globals.css";
 
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
 export const metadata = {
     title: "Promptopia",
     description: "Discover & Share AI Prompts",
 };
 
-function RootLayout({children}) {
+function RootLayout({ children }) {
     return (
         <html lang="en">
             <body>
-                <div className="main">
-                    <div className="gradient" />
-                </div>
-
-                <main className="app">{children}</main>
+                <Provider>
+                    <div className="main">
+                        <div className="gradient" />
+                    </div>
+                    <main className="app">
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
             </body>
         </html>
     );
