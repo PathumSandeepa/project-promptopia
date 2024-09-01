79b992c12d91c4f05489734660bf0a471894ebd0
Pathum Sandeepa <pathumsandeepa13@gmail.com>
2024-09-01 05:31:12 +0530

    adding call back, optimized routes

--- app/api/auth/[...nextauth]/route.jsx
+++ app/api/auth/[...nextauth]/route.jsx
@@ -1,13 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
-
 import User from "@models/user";
 import { connectToDB } from "@utils/database";
-
-console.log({
-    clientId: process.env.GOOGLE_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-});
 
 const handler = NextAuth({
     providers: [
@@ -16,36 +10,43 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
     ],
-    async session({ session }) {
-        const sessionUser = await User.findOne({
-            email: session.user.email,
-        });
-
-        session.user.id = sessionUser._id.toString();
-
-        return session;
-    },
-    async signIn({ profile }) {
-        try {
-            await connectToDB();
-
-            const userExists = await User.findOne({
-                email: profile.email,
-            });
-
-            if (!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture,
+    callbacks: {
+        async session({ session }) {
+            try {
+                await connectToDB();
+                const sessionUser = await User.findOne({
+                    email: session.user.email,
                 });
-            }
 
-            return true;
-        } catch (error) {
-            console.log(error);
-            return false;
-        }
+                if (sessionUser) {
+                    session.user.id = sessionUser._id.toString();
+                }
+
+                return session;
+            } catch (error) {
+                console.error("Error fetching user for session:", error);
+                return session;
+            }
+        },
+        async signIn({ profile }) {
+            try {
+                await connectToDB();
+                const userExists = await User.findOne({ email: profile.email });
+
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture,
+                    });
+                }
+
+                return true;
+            } catch (error) {
+                console.error("Error during sign-in:", error);
+                return false;
+            }
+        },
     },
 });
 
